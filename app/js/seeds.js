30496594fea6adc52ba263e1e0d59710f2fcf303
Agata Liberska <liberska@gmail.com>
2021-03-10 22:34:14 +0000

    add testimonials

--- app/js/seeds.js
+++ app/js/seeds.js
@@ -13,6 +13,27 @@ const testimonials = [
         image: '/images/image-john.jpg',
         alt: 'John Tarkpor',
     },
+    {
+        name: 'Olivia Pope',
+        role: 'Junior JavaScript Developer',
+        text: `“This course changed my life! I've tried learning to code on my own, but kept getting lost in the sea of material available online. With this course, I learnt more in 3 months than in the 2 years before!”`,
+        image: '/images/image-olivia.jpg',
+        alt: 'Olivia Pope',
+    },
+    {
+        name: 'Jenna Shaw',
+        role: 'Junior Full-stack Developer',
+        text: `“This course will give you everything you need to get your first job in web development. The instructors are fantastic at explaining the most difficult concepts and teaching assistants are always there to help out.”`,
+        image: '/images/image-jenna.jpg',
+        alt: 'Jenna Shaw',
+    },
+    {
+        name: 'Ian Smith',
+        role: 'Full-stack Developer',
+        text: `“I was a bit hesitant about joining this course but after the first week, all my doubts were gone. I got my first job offer before the course ended, and looking back, I know I was very well prepared for it.”`,
+        image: '/images/image-ian.jpg',
+        alt: 'Ian Smith',
+    },
 ]
 
 export { testimonials };
